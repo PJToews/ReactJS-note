221d7e9493c5f7498efb1e4e823890de5fd5ec97
PJToews <lohasjean@gmail.com>
2019-07-05 04:18:09 +0000

    Add Toggling Ability

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
@@ -14,12 +14,18 @@ class App extends Component {
     };
   }
 
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote
+    });
+  }
+
   render() {
     const { showNote } = this.state;
 
     return (
       <div className="App">
-        <Nav />
+        <Nav toggleNote={this.toggleNote} showNote={showNote} />
         { showNote ? <Note /> : <List /> }     
       </div>
      );
