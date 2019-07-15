2db6b6fbff7f32b8f48c35699267c04e30a2fa68
PJToews <lohasjean@gmail.com>
2019-07-15 03:08:08 +0000

    Setting the data of the note display in the form

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 class NoteCard extends React.Component {
   render() {
-    const { note } = this.props;
+    const { note, getNote } = this.props;
 
     return (
       <div className="note-card-container">
         <div className="note-card-title">
-         {note.title}
-         </div>
-         <div className="note-card-content">
+          {note.title}
+        </div>
+        <div className="note-card-content">
           {note.content}
-          </div>
-          <span className="note-card-delete">
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">close</i>
+        </span>
+        <span className="note-card-edit" onClick={() => getNote(note.id)}>
           <i className="material-icons">mode_edit</i>
-          </span>
+        </span>
       </div>
     );
   }
