3fed574832725171766dc912d4412d3c07d0d83e
PJToews <lohasjean@gmail.com>
2019-07-17 23:54:11 +0000

    defined renderTagForm and updated Notecard.js

--- src/helpers/urlFor.js
+++ src/helpers/urlFor.js
@@ -1,5 +1,5 @@
 const urlFor = (endpoint) => {
-  return 'https://note-api-peijean.herokuapp.com/notes' + endpoint;
+  return 'https://note-api-peijean.herokuapp.com/' + endpoint;
 };
 
 export default urlFor;
